98ac9be32e6a524194dcf306f8a93316e4cda1fe
utrodus <come.programmer@gmail.com>
2019-12-27 14:11:20 +0700

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ipa/screen/pages/mainmenu/screen.dart';
-import 'package:ipa/screen/style/palete.dart';
 
 void main() => runApp(MyApp());
 
