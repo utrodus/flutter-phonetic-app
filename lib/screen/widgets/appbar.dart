53877966ff82500063ae12ec6e2866171eab9327
utrodus <come.programmer@gmail.com>
2019-12-28 22:23:18 +0700

    delete unused image assets & restyling appbar

--- lib/screen/widgets/appbar.dart
+++ lib/screen/widgets/appbar.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:ipa/screen/style/config.dart';
 import 'package:ipa/screen/style/palete.dart';
 
 PreferredSizeWidget appBar (BuildContext context, {String judul}){
+  SizeConfig().init(context);
     return AppBar(
       centerTitle: true,
-      backgroundColor: Palete.blue,
+      backgroundColor: Palete.bgAppbar,
       title: Text(
         judul,
         style: TextStyle(
           color:Palete.white,
-
+          fontFamily: Palete.cabinRegular,
+          letterSpacing: 1,
+          fontSize: SizeConfig.horizontal * 4.8
         ),
       ),
     );
