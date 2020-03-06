dd9faa67012bd3cabd9af14e29dbed971c161c7f
dieasquo <dieasquo@gmail.com>
2020-03-06 23:46:09 +0700

    Tambah Konten r (misspronounce)

--- lib/screen/pages/phonetic/wrong/r/contentwrong.dart
+++ lib/screen/pages/phonetic/wrong/r/contentwrong.dart
@@ -22,9 +22,27 @@ class ContentWrong extends StatelessWidget {
           ContentLayoutWrong(
             visible: wrongState.getWrongContent == 0 ? true : false,
             symbol: simbol,
+            viewSifat: true,
             img: "assets/img/wrong/r.png",
+            sifat: "Apical Alveolar Trill",
+            content: Column(
+              children: <Widget>[
+                penjelasan(context,
+                    number: "1. ",
+                    text:
+                        "Slightly round the lips."),
+                penjelasan(context,
+                    number: "2. ",
+                    text: "Put the tip of the tongue at the alveolar ridge."),
+                penjelasan(context,
+                    number: "3. ", text: "Curl back the tongue."),
+                penjelasan(context,
+                    number: "4. ", text: "Vibrate/trill the tongue while pushing the air"),
+                penjelasan(context,
+                    number: "5. ", text: "Vibrate the vocal cords."),
+              ],
+            ),
           ),
-          
         ],
       ),
     );
